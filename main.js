54fcbb2ff3c90dbc3628f7d0f83fbea7336813c2
dest411 <vadimstrizhak652@gmail.com>
2025-07-05 13:50:23 +0300

    comittt

---
+++ main.js
@@ -0,0 +1,33 @@
+function toggleBtnTheme(element) {
+    element.classList.toggle("dark");
+  }
+function toggleList() {
+  const list = document.getElementById('birdlist-where');
+  if (list.style.display === 'block') {
+    list.style.display = 'none';
+  } else {
+    list.style.display = 'block';
+  }
+}
+function toggleList2() {
+  const list = document.getElementById('birdlist-date');
+  if (list.style.display === 'block') {
+    list.style.display = 'none';
+  } else {
+    list.style.display = 'block';
+  }
+}
+function toggleList3() {
+  const list = document.getElementById('birdlist-people');
+  if (list.style.display === 'block') {
+    list.style.display = 'none';
+  } else {
+    list.style.display = 'block';
+  }
+}
+
+function smash (words) {
+  
+   const arr = [];
+   
+};
